81378528d9c02e8a892fe1c0cc32b82c2a8c8381
Dave Lee <leezhi131@gmail.com>
2020-12-22 11:22:47 +0800

    add python batter PascalsTriangle

--- python/array/PascalsTriangle.py
+++ python/array/PascalsTriangle.py
@@ -29,27 +29,42 @@ class PascalsTriangleSolution:
     """
 
     def generate(self, numRows: int) -> List[List[int]]:
-        res = []
-        pre = None
-        for i in range(numRows):
-            if i == 0:
-                one = [1]
-                res.append(one)
-                pre = one
-            elif i == 1:
-                two = [1, 1]
-                res.append(two)
-                pre = two
-            else:
-                arr = []
-                for j in range(i + 1):
-                    if j == 0 or j == i:
-                        arr.append(1)
-                    else:
-                        arr.append(pre[j] + pre[j - 1])
-                res.append(arr)
-                pre = arr
-        return res
+        # res = []
+        # pre = None
+        # for i in range(numRows):
+        #     if i == 0:
+        #         one = [1]
+        #         res.append(one)
+        #         pre = one
+        #     elif i == 1:
+        #         two = [1, 1]
+        #         res.append(two)
+        #         pre = two
+        #     else:
+        #         arr = []
+        #         for j in range(i + 1):
+        #             if j == 0 or j == i:
+        #                 arr.append(1)
+        #             else:
+        #                 arr.append(pre[j] + pre[j - 1])
+        #         res.append(arr)
+        #         pre = arr
+        # return res
+
+        triangle = []
+        for row_num in range(numRows):
+            # The first and last row elements are always 1.
+            row = [None for _ in range(row_num + 1)]
+            row[0], row[-1] = 1, 1
+
+            # Each triangle element is equal to the sum of the elements
+            # above-and-to-the-left and above-and-to-the-right.
+            for j in range(1, len(row) - 1):
+                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
+
+            triangle.append(row)
+
+        return triangle
 
 
 solution = PascalsTriangleSolution()

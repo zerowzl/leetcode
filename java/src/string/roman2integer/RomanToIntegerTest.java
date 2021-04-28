fdf6a351d1ad60732a6cb2ae0902466e6672a497
davewang <davewang1024@gmail.com>
2021-04-28 22:43:51 +0800

    update 13 solution

---
+++ java/src/string/roman2integer/RomanToIntegerTest.java
@@ -0,0 +1,22 @@
+package string.roman2integer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author davewang
+ */
+public class RomanToIntegerTest {
+
+
+    @Test
+    public void test() {
+        RomanToInteger romanToInteger = new RomanToInteger();
+        Assert.assertEquals(3, romanToInteger.romanToInt("III"));
+        Assert.assertEquals(4, romanToInteger.romanToInt("IV"));
+        Assert.assertEquals(14, romanToInteger.romanToInt("XIV"));
+        Assert.assertEquals(30, romanToInteger.romanToInt("XXX"));
+    }
+
+
+}

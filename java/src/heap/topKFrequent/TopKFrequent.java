ad15d9c275b5639b94aaf41a8da43d419e9058f7
davewang <davewang1024@gmail.com>
2021-04-30 17:12:15 +0800

    update 347 solution

--- java/src/heap/topKFrequent/TopKFrequent.java
+++ java/src/heap/topKFrequent/TopKFrequent.java
@@ -22,10 +22,23 @@ k 的取值范围是 [1, 数组中不相同的元素的个数]
 
 *******************************************************************************/
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
+/* *****************************************************************************
+
+题解：
+先遍历数组，统计元素出现次数；
+构建 k 大小的小顶堆，堆内元素类型使用 数组，数组第一个元素是数值，第二个元素是数值出现的次数，排序规则：比较出现次数；
+让堆中插入；
+根据堆构建返回结果。
+
+复杂度分析：
+时间复杂度：首先，遍历一次数组的时间复杂度是 O(n)；
+随后，遍历「出现次数数组」，由于堆的大小至多为 k，因此每次堆操作需要 O(log k) 的时间，共需 O(N log k) 的时间。
+二者之和为 O(N log k)。
+空间复杂度：O(N)。哈希表的大小为 O(N)，而堆的大小为 O(k)，共计为 O(N)。
+
+*******************************************************************************/
+
+import java.util.*;
 
 /**
  * @author Dave Wang
@@ -36,43 +49,27 @@ public class TopKFrequent {
         // 统计元素出现的次数
         Map<Integer, Integer> countMap = new HashMap<>();
         for (int num : nums) {
-            if (countMap.containsKey(num)) {
-                countMap.put(num, countMap.get(num) + 1);
-            } else {
-                countMap.put(num, 1);
-            }
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
         }
 
-        // 构建一个小顶堆，堆内存的是元素出现的次数
-        Queue<Integer> head = new PriorityQueue<>(k);
+        // 构建一个小顶堆，堆内存的是元素和元素出现次数
+        Queue<int[]> head = new PriorityQueue<>(k, Comparator.comparingInt(o -> o[1]));
         for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
             if (head.size() < k) {
-                head.add(entry.getValue());
+                head.add(new int[]{entry.getKey(), entry.getValue()});
             } else {
-                if (entry.getValue() > head.peek()) {
+                if (entry.getValue() > head.peek()[1]) {
                     head.poll();
-                    head.add(entry.getValue());
+                    head.add(new int[]{entry.getKey(), entry.getValue()});
                 }
             }
         }
 
+        // 取出堆中元素数组的第一位构建返回结果
         int[] res = new int[k];
-        int i = 0;
-        // 循环删除堆顶元素
-        while (!head.isEmpty()) {
-            Integer poll = head.poll();
-            // 如果统计 map 的中出现元素次数和堆顶元素一致，就将 key（元素值）加入到返回结果中，
-            // 并跳出循环（防止出现元素出现次数相同导致重复添加）
-            for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
-                if (entry.getValue().equals(poll)) {
-                    res[i] = entry.getKey();
-                    i++;
-                    countMap.remove(entry.getKey());
-                    break;
-                }
-            }
+        for (int i = 0; i < k; i++) {
+            res[i] = head.poll()[0];
         }
-
         return res;
     }
 

e24fc6fd97da9b5fe32575d5bff6613de075807a
davewang <davewang1024@gmail.com>
2021-04-30 16:56:28 +0800

    fix 347 solution

--- java/src/heap/topKFrequent/TopKFrequent.java
+++ java/src/heap/topKFrequent/TopKFrequent.java
@@ -33,6 +33,7 @@ import java.util.Queue;
 public class TopKFrequent {
 
     public int[] topKFrequent(int[] nums, int k) {
+        // 统计元素出现的次数
         Map<Integer, Integer> countMap = new HashMap<>();
         for (int num : nums) {
             if (countMap.containsKey(num)) {
@@ -42,22 +43,36 @@ public class TopKFrequent {
             }
         }
 
+        // 构建一个小顶堆，堆内存的是元素出现的次数
         Queue<Integer> head = new PriorityQueue<>(k);
         for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
             if (head.size() < k) {
-                head.add(entry.getKey());
+                head.add(entry.getValue());
             } else {
-                if (entry.getValue() > countMap.get(head.peek())) {
+                if (entry.getValue() > head.peek()) {
                     head.poll();
-                    head.add(entry.getKey());
+                    head.add(entry.getValue());
                 }
             }
         }
 
         int[] res = new int[k];
-        for (int i = 0; i < k; i++) {
-            res[i] = head.poll();
+        int i = 0;
+        // 循环删除堆顶元素
+        while (!head.isEmpty()) {
+            Integer poll = head.poll();
+            // 如果统计 map 的中出现元素次数和堆顶元素一致，就将 key（元素值）加入到返回结果中，
+            // 并跳出循环（防止出现元素出现次数相同导致重复添加）
+            for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+                if (entry.getValue().equals(poll)) {
+                    res[i] = entry.getKey();
+                    i++;
+                    countMap.remove(entry.getKey());
+                    break;
+                }
+            }
         }
+
         return res;
     }
 
--- java/src/heap/topKFrequent/TopKFrequentTest.java
+++ java/src/heap/topKFrequent/TopKFrequentTest.java
@@ -12,7 +12,8 @@ public class TopKFrequentTest {
     @Test
     public void topKFrequent() {
         TopKFrequent solution = new TopKFrequent();
-        assertArrayEquals(new int[]{1, 2}, solution.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2));
+        assertArrayEquals(new int[]{2, 1}, solution.topKFrequent(new int[]{1, 1, 1, 2, 2, 3}, 2));
         assertArrayEquals(new int[]{1}, solution.topKFrequent(new int[]{1}, 1));
+        assertArrayEquals(new int[]{-1, 2}, solution.topKFrequent(new int[]{4, 1, -1, 2, -1, 2, 3}, 2));
     }
 }

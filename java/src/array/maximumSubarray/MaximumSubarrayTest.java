49694d70fd7ee5b1849c252a12708a9c6cd013a4
davewang <davewang1024@gmail.com>
2021-07-15 14:13:09 +0800

    Add No.53 MaximumSubarray of Java

---
+++ java/src/array/maximumSubarray/MaximumSubarrayTest.java
@@ -0,0 +1,22 @@
+package array.maximumSubarray;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Dave Wang
+ */
+public class MaximumSubarrayTest {
+
+    private MaximumSubarray solution = new MaximumSubarray();
+
+    @Test
+    public void maxSubArray() {
+        assertEquals(6, solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+        assertEquals(6, solution.maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1}));
+        assertEquals(4, solution.maxSubArray(new int[]{-2, 1, -3, 4}));
+        assertEquals(7, solution.maxSubArray(new int[]{-2, 1, 4, -1, 2, 1, -5, 4}));
+        assertEquals(-2, solution.maxSubArray(new int[]{-2}));
+    }
+}

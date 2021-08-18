03c1dcff93d15694f35c84294b8e4d651ecb3ec3
davewang <davewang1024@gmail.com>
2021-08-18 21:48:15 +0800

    Add No.117 PopulatingNextRightPointersInEachNode2 solution of java

---
+++ java/src/dbsf/populatingNextRightPointersInEachNode2/PopulatingNextRightPointersInEachNode2.java
@@ -0,0 +1,93 @@
+package dbsf.populatingNextRightPointersInEachNode2;
+
+/* *****************************************************************************
+
+117. 填充每个节点的下一个右侧节点指针 II
+给定一个二叉树
+
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+
+填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+初始状态下，所有 next 指针都被设置为 NULL。
+
+进阶：
+你只能使用常量级额外空间。
+使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+
+示例：
+输入：root = [1,2,3,4,5,null,7]
+输出：[1,#,2,3,#,4,5,7,#]
+解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化输出按层序遍历顺序（由 next 指针连接），'#' 表示每层的末尾。
+
+提示：
+树中的节点数小于 6000
+-100 <= node.val <= 100
+
+*******************************************************************************/
+
+/* *****************************************************************************
+
+题解：
+广度优先算法
+
+复杂度分析：
+时间复杂度：O(n)
+空间复杂度：O(n)
+
+*******************************************************************************/
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @author Dave Wang
+ */
+public class PopulatingNextRightPointersInEachNode2 {
+
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+
+        // 初始化队列同时将第一层节点加入队列中，即根节点
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
+
+        // 外层的 while 循环迭代的是层数
+        while (!queue.isEmpty()) {
+
+            // 记录当前队列大小
+            int size = queue.size();
+
+            // 遍历这一层的所有节点
+            for (int i = 0; i < size; i++) {
+
+                // 从队首取出元素
+                Node node = queue.poll();
+
+                // 连接
+                if (i < size - 1) {
+                    node.next = queue.peek();
+                }
+
+                // 拓展下一层节点
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+
+        // 返回根节点
+        return root;
+    }
+
+}
